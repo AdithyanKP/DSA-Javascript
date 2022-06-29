dbf131fe37a1296183419fbf7694705cca5702d5
Adithyan k P <adithyankp777@gmail.com>
2022-06-29 13:44:12 +0530

    bubble sort algorithem

--- bubblesort.js
+++ bubblesort.js
@@ -1,8 +1,23 @@
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        console.log("swapped");
+        swapped = true;
+        console.log("completed");
+      }
+    }
+  } while (swapped === true);
 
+  console.log(arr);
+  return arr;
+}
 
+let arry = [1, 2, 3];
 
-
-
-let arry =[2,3,7,8,-2]
-
-function bubbleSort(arry);
+bubbleSort(arry);
